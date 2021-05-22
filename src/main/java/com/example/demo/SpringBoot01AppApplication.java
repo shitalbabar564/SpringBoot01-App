7667ab0890505bdebbc0f529559883fd6b28829c
Shital <shitalbabar08@gmail.com>
2021-05-22 14:47:04 +0530

    Initial Commit

---
+++ src/main/java/com/example/demo/SpringBoot01AppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot01AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot01AppApplication.class, args);
+	}
+
+}
